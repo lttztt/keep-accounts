51c04d2e03d1032d29f1afdce1c04fa00a34ab35
liuzhe <liuzhexx@foxmail.com>
2020-02-09 13:16:01 +0800

    feat: 用 value 和 input事件 完成备注组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="notes">
     <label>
+      {{value}}
       <span class="name">备注</span>
-      <input type="text" placeholder="在这里添加备注">
+      <input type="text"
+             v-bind:value="value"
+             @input="onInput"
+             placeholder="在这里添加备注">
     </label>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: "Notes"
-  };
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Notes extends Vue {
+    value = '';
+    onInput(event: KeyboardEvent){
+      const target = event.target as HTMLInputElement;
+      if(target){
+        this.value = target.value;
+      }
+    }
+  }
 </script>
 
 
