060d56d46a2f1befea4c03956e7b8161259497c2
liuzhe <liuzhexx@foxmail.com>
2020-05-26 17:05:44 +0800

    删除money/types.vue

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="tabs">
-    <ul>
-      <li
-        v-for="item in dataSource"
-        :key="item.value"
-        :class="liClass(item)"
-        @click="selectTab(item)">{{item.text}}
-      </li>
-    </ul>
-  </div>
+  <ul class="tabs">
+    <li
+      v-for="item in dataSource"
+      :key="item.value"
+      :class="liClass(item)"
+      @click="selectTab(item)">{{item.text}}
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component, Prop, Watch} from 'vue-property-decorator';
-  type DataSourceType = {text: string, value: string}
+  import {Component, Prop} from 'vue-property-decorator';
+
+  type DataSourceType = { text: string, value: string }
 
   @Component
   export default class Tabs extends Vue {
@@ -22,19 +21,15 @@
     @Prop(String) readonly value!: string;
     @Prop(String) classPrefix?: string;
 
-    @Watch('dataSource')
-    onSourceChange(v: DataSourceType){
-      console.log('v', v);
-    }
-    liClass(tab: DataSourceType){
+    liClass(tab: DataSourceType) {
       return {
         [this.classPrefix + '-tabs-item']: this.classPrefix,
         selected: tab.value === this.value
-      }
+      };
     }
 
     selectTab(tab: DataSourceType) {
-      this.$emit('update:value', tab.value)
+      this.$emit('update:value', tab.value);
     }
   }
 </script>
@@ -42,32 +37,30 @@
 
 <style scoped lang="scss">
   .tabs {
-    ul {
-      color: #000;
-      background: #c4c4c4;
+    color: #000;
+    background: #c4c4c4;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    li {
+      height: 64px;
+      width: 50%;
       display: flex;
       justify-content: center;
       align-items: center;
+      position: relative;
+      font-size: 24px;
 
-      li {
-        height: 64px;
-        width: 50%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-        font-size: 24px;
-
-        &.selected {
-          &::after {
-            content: '';
-            position: absolute;
-            left: 0;
-            bottom: 0;
-            height: 4px;
-            width: 100%;
-            background: #000;
-          }
+      &.selected {
+        &::after {
+          content: '';
+          position: absolute;
+          left: 0;
+          bottom: 0;
+          height: 4px;
+          width: 100%;
+          background: #000;
         }
       }
     }
